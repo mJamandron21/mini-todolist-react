fb9532a914b164f2d239234c4b7fd1ea38891e18
Mark Kenneth Jamandron <mjamandron@gmail.com>
2022-01-15 11:22:50 +0800

    Update: todolist done!

---
+++ src/components/Todo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'Todo-row Todo-complete' : 'Todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='Icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='Delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='Edit-icon'
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
